af10ad0ffb131f3d9ad27234e41cbf4425490058
Will Forbes <wforbes87@gmail.com>
2021-11-15 20:38:39 -0800

    customize the home page
    - video #5 https://vueschool.io/lessons/customize-the-home-page

---
+++ components/WfLogo.vue
@@ -0,0 +1,3 @@
+<template>
+  	<img src="../assets/wf_main.png" alt="main wforbes.com image with picture of Will Forbes" />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <WfLogo />
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+</style>
