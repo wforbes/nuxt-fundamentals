a606bf5519dec96c00a6f4ff2bd9d5e7e5e85988
Will Forbes <wforbes87@gmail.com>
2021-11-15 21:09:36 -0800

    Layout page like tutorial
    - nuxt tutorial im following is old, added layout file from their repo at https://github.com/vueschool/nuxt-fundamentals/blob/298e0873112410afcf881bde62dca93e1138795b/pages/index.vue
    - added a sketchy logo and styled page to match the tutorial

--- components/WfLogo.vue
+++ components/WfLogo.vue
@@ -1,3 +1,10 @@
 <template>
-  	<img src="../assets/wf_main.png" alt="main wforbes.com image with picture of Will Forbes" />
+  	<img src="../assets/wf_sketch_logo.png" alt="main wforbes.com image with picture of Will Forbes" />
 </template>
+
+<style scoped>
+	img {
+		margin:0;
+		padding:0;
+	}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="container">
-    <WfLogo />
-  </div>
+	<section class="container">
+		<div class="jumbo">
+	    	<WfLogo />
+		</div>
+		<h1 class="title">
+			will forbes
+		</h1>
+		<h2 class="sub-title">
+			nuxt-fundamentals site
+		</h2>
+		<div class="links">
+			<a
+				href="#"
+				class="button--grey"
+			>
+				About
+			</a>
+			<a
+				href="#"
+				class="button--green"
+			>
+				Documentation
+			</a>
+		</div>
+  	</section>
 </template>
 
 <script>
@@ -10,9 +32,31 @@ export default {}
 
 <style scoped>
 .container {
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+		'Segoe UI', Roboto;
+	min-height: 100vh;
 	text-align: center;
+}
+.jumbo {
+	margin-top: 3em;
+	display:block;
+}
+.title {
+	color: #7e61f3;
+	display: block;
+	font-weight: 700;
+	font-size: 75px;
+	letter-spacing: 1px;
+	margin: 0;
+	padding: 0;
+}
+.sub-title {
+	color: #26add4;
+	margin-top: 0;
+	display: block;
+}
+.links {
+	display: block;
+	padding-top: 1em;
 }
 </style>
